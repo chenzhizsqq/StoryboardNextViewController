b64e398c5c08e1db291e4fd4d6551d61df9d03f7
chenzhizs <chenzhi@qq.com>
2022-06-06 18:31:34 +0900

    “从下往上弹出”的demo

--- StoryboardNextViewController/ViewController.swift
+++ StoryboardNextViewController/ViewController.swift
@@ -9,9 +9,24 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var checkInBtn: UIButton!
+    
+    
+    //从下往上弹出
+    @IBAction func checkInBtnClick(_ sender: UIButton) {
+        
+        print("从下往上弹出")
+        
+        
+        
+        let nextViewController = self.storyboard?.instantiateViewController(withIdentifier: "ViewController2") as! ViewController2
+        self.present(nextViewController, animated: true, completion: nil)
+        
+    }
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        
     }
 
 
