a72a55901248818b63744daad60ee61312b22d42
chenzhizs <chenzhi@qq.com>
2022-06-07 16:38:25 +0900

    做好了一个完整的简单的页面了

--- StoryboardNextViewController/ViewController.swift
+++ StoryboardNextViewController/ViewController.swift
@@ -23,6 +23,21 @@ class ViewController: UIViewController {
         self.present(nextViewController, animated: true, completion: nil)
         
     }
+    
+    //新的页面
+    @IBAction func checkInBtnClick2(_ sender: UIButton) {
+        print("checkInBtnClick2")
+        
+        
+        let nextViewController = self.storyboard?.instantiateViewController(withIdentifier: "ViewController2") as! ViewController2
+        nextViewController.modalPresentationStyle = .fullScreen // ★この行追加
+        self.present(nextViewController, animated: true, completion: nil)
+        
+        
+        
+    }
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
--- StoryboardNextViewController/ViewController2.swift
+++ StoryboardNextViewController/ViewController2.swift
@@ -9,21 +9,14 @@ import UIKit
 
 class ViewController2: UIViewController {
 
+    
+    @IBAction func goBack(_ sender: Any) {
+        self.dismiss(animated: true, completion: nil)
+    }
     override func viewDidLoad() {
         super.viewDidLoad()
 
         // Do any additional setup after loading the view.
     }
-
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
-    }
-    */
 
 }
